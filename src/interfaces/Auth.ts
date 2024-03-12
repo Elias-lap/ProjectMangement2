7c19c397e84330605447c74aaea6559fd0d28021
Elias-lap <Eliaszerz92@gmail.com>
2024-03-12 01:14:06 +0100

    designTest

--- src/interfaces/Auth.ts
+++ src/interfaces/Auth.ts
@@ -5,15 +5,19 @@ export interface FormData {
   confirmPassword?: string;
   seed?: string;
 }
+export interface FormDataRegister {
+  userName: string;
+  email : string;
+  country : string;
+  phoneNumber : string;
+  password : string;
+  confirmPassword  : string;
+}
 export interface ToastContextType {
   showToast: (message: string) => void;
   showSuccessToast: (message: string) => void;
   showErrorToast: (message: string) => void;
 }
 
-// export interface Authcontext {
-//   setAdminData: () => void;
-//   adminData?: string;
-// }
 
 
