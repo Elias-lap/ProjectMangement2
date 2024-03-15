bc269f6e197320ed271f14e5a3b8cb46565f1065
AhmedYLasheen <ay514092@gmail.com>
2024-03-16 01:43:55 +0200

    edit

--- src/Dashboard/components/Dashboard/Dashboard.tsx
+++ src/Dashboard/components/Dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ import Header from '../Header/Header'
 export default function Dasxboard() {
   return (
     <>
-    <h1>ddddddddddddd</h1>
+    {/* <h1>ddddddddddddd</h1> */}
     <Header/>
 
     </>
